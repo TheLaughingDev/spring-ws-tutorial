a79469f63d1b1306408e5a6245b009781d85cb43
Justin <jbcarpenter1@gmail.com>
2018-05-12 10:54:26 -0400

    Updating to java 10.

--- server/src/main/kotlin/org/thelaughingdev/soap/server/repository/CountryRepository.kt
+++ server/src/main/kotlin/org/thelaughingdev/soap/server/repository/CountryRepository.kt
@@ -1,7 +1,6 @@
 package org.thelaughingdev.soap.server.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.thelaughingdev.wsdl.Currency
 import org.springframework.stereotype.Repository
 import org.thelaughingdev.soap.server.domain.Country
 
